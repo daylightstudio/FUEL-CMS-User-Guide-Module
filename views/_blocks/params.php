1aa8ccfbd9e31bdcbfff383a72138156b070ba25
David McReynolds <dave@thedaylightstudio.com>
2012-03-06 14:35:14 -0800

    update params block

--- views/_blocks/params.php
+++ views/_blocks/params.php
@@ -1,23 +1,27 @@
 <?php if (!empty($params)) : ?>
+<?php 
+foreach($params as $key => $param) :
+
+$comment_param = (isset($comment_params[$key])) ? $comment_params[$key] : '';
+$param_comment = '';
+$param_type = '';
+if (isset($comment_params[$key]))
+{
+	preg_match('#(\w+)\s+(.+)#', $comment_param, $matches);
+	$param_type = (isset($matches[1])) ? $matches[1] : '';
+	$param_comment = (isset($matches[2])) ? $matches[2] : '';
+	
+	if (!empty($param_type)) $str = "(";
+	$str = $param_type;
+	if (!empty($param_type)) $str = ") ";
+	$str = "<strong>$".$param->name."</strong> ".$param_comment."\n";
+}
+endforeach;
+?>
+<?php if (!empty($str)) : ?>
 <h4>Parameters</h4>
 <pre>
-<?php 
-if (!empty($params))
-{
-	foreach($params as $key => $param) :
-	
-	$comment_param = (isset($comment_params[$key])) ? $comment_params[$key] : '';
-	$param_comment = '';
-	$param_type = '';
-	if (isset($comment_params[$key]))
-	{
-		preg_match('#(\w+)\s+(.+)#', $comment_param, $matches);
-		$param_type = (isset($matches[1])) ? $matches[1] : '';
-		$param_comment = (isset($matches[2])) ? $matches[2] : '';
-	}
-	echo"(".$param_type.") <strong>$".$param->name."</strong> ".$param_comment."\n";
-	endforeach;
-}
-?>
+<?=$str?>
 </pre>
+<?php endif; ?>
 <?php endif; ?>
