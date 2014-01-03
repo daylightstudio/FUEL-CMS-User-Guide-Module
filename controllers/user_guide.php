8933adb3fdade86704aa261317fd612bcd135116
David McReynolds <dave@thedaylightstudio.com>
2014-01-03 13:49:37 -0800

    fixed documentation link in user_guide controller

--- controllers/user_guide.php
+++ controllers/user_guide.php
@@ -23,7 +23,7 @@
  * @subpackage	Libraries
  * @category	Libraries
  * @author		David McReynolds @ Daylight Studio
- * @link		http://www.getfuelcms.com/user_guide/modules/user_guide
+ * @link		http://docs.getfuelcms.com/modules/user_guide
  */
 
 // --------------------------------------------------------------------
