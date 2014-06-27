813f8ece328cf223686f032a750c3a8e1ac03337
David McReynolds <dave@thedaylightstudio.com>
2014-06-27 11:44:32 -0700

    text update for the config block

--- views/_blocks/config.php
+++ views/_blocks/config.php
@@ -1,6 +1,7 @@
 <?php if ($config) : ?>
 <h2><?=humanize($module)?> Configuration</h2>
-<p>The following configuration parameters can be found in the <dfn>modules/<?=$module?>/config/<?=$module?>.php</dfn> configuration file.</p>
+<p>The following configuration parameters can be found in the <dfn>modules/<?=$module?>/config/<?=$module?>.php</dfn> configuration file. 
+	It is recommended that you copy the config file and place it in your <dfn>fuel/application/config</dfn> directory which will override the defaults and make it easier for future updates.</p>
 <table border="0" cellspacing="1" cellpadding="0" class="tableborder">
 	<tbody>
 		<tr>
